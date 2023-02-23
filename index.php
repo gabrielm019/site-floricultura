46e64ff38d9ed3bf33b6672e28627bb16f9fc460
Gabriel Augusto <110576161+gabrielm019@users.noreply.github.com>
2023-02-22 22:17:58 -0300

    exemplo

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">FLORICULTURA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PRODUTO</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            CONTATO
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="FLOR 1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="FLOR2.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="..." class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+
+                <div class="card" style="width: 18rem;">
+                    <img src="FLOR2.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+
+                        <h5 class="card-title">GIRASSOL</h5>
+                        <p class="card-text">Girassol é uma planta anual com caule grosso, robusto, erecto, simples e de cor verde médio. Podem atingir alturas de 2-3 m, havendo variedades anãs de 40 cm de altura. As folhas são ovais, opostas, pecioladas, com nervuras bem marcadas, ásperas ao tacto com cerca de 15-30 cm de tamanho.</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+
+                <div class="card" style="width: 18rem;">
+                    <img src="FLOR 1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+
+                        <h5 class="card-title text-success">MARGARIDA</h5>
+                        <p class="card-text">De origem europeia e repleta de características marcantes, a margarida apresenta pétalas alargadas e delgadas que contornam o botão central, dourado ou amarelo. Suas folhas são ovais e seu caule é comprido, podendo chegar até 1 metro de altura, quando cultivado no jardim</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col">
+
+                <div class="card" style="width: 18rem;">
+                    <img src="FLOR 1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+
+                        <h5 class="card-title text-success">MARGARIDA</h5>
+                        <p class="card-text">De origem europeia e repleta de características marcantes, a margarida apresenta pétalas alargadas e delgadas que contornam o botão central, dourado ou amarelo. Suas folhas são ovais e seu caule é comprido, podendo chegar até 1 metro de altura, quando cultivado no jardim</p>
+                        <a href="#" class="btn btn-primary">COMPRAR</a>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+                    <h2 class="display-6 text-center">CONTATO</h2>
+
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3679.328661911507!2d-47.331282684888365!3d-22.753181138128557!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8998174c896b9%3A0x739b7f99b3323574!2sSenac%20Americana!5e0!3m2!1spt-BR!2sbr!4v1677113318137!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+
+
+
+
+        <footer class="container bg-dark">
+            <div class="row">
+                <div class="col">
+
+
+
+                </div>
+
+
+
+            </div>
+
+
+
+        </footer>
+
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
